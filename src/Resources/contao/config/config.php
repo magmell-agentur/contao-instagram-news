6bbca26b2bcacc5e840d0b09f993b8116ea0e598
Markus Milkereit <markus.milkereit@magmell.de>
2023-03-30 17:10:43 +0200

    compatability update for stricter checks in PHP 8.x

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['TL_CRON']['hourly'][] = ['Magmell\Contao\InstagramNews\InstagramNewsImporter', 'run'];
 
-if ($_COOKIE['XDEBUG_SESSION'] === 'PHPSTORM_MARKO')
+if (isset($_COOKIE['XDEBUG_SESSION']) && $_COOKIE['XDEBUG_SESSION'] === 'PHPSTORM_MARKO')
 {
     $GLOBALS['TL_HOOKS']['generatePage'][] = ['Magmell\Contao\InstagramNews\InstagramNewsImporter', 'run'];
 }
